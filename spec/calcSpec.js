74de599a7c60bccf6d9e3cb77e27b97916c63fd9
Harvin Zúniga <harvin.zuniga@gmail.com>
2020-08-03 10:03:22 +0000

    Adding Each Spec

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,13 +1,40 @@
 describe("Calculator", function(){
+    beforeEach(function(){
+        calc = new Calculator;
+    });
+
+    /*Refactoring */
+   /* var calc = new Calculator;*/
+
     describe("Addition test", function (){
         it ("should return 42", function(){ 
-            expect(addition(20, 22)).toBe(42);
+            calc.add(20);
+            calc.add(22);
+            expect(calc.value).toBe(42);
         });
         it ("should return 26", function(){
-            expect(addition(7, 19)).toBe(26);
+            calc.add(7);
+            calc.add(19);
+            expect(calc.value).toBe(26);
         });
         it ("should return error if we don´t supply two numbers", function(){
-            expect(addition("Hitchhickers", "Guide")).toBe("Error!");
+            spyOn(window, "alert");
+            calc.add("Hitchhikers");
+            expect(window.alert).toHaveBeenCalledWith("Error!");
         })
+        /*Spies and Programming Defensive
+
+        it ("should return error if we don´t supply two numbers", function(){
+            spyOn(window, "alert");
+            addition("Hitchhikers", "Guide")
+            expect(window.alert).toHaveBeenCalledWith("Error!");
+        })*/
+        
+        /*
+        Programming Defensive
+        
+        it ("should return error if we don´t supply two numbers", function(){
+            expect(addition("Hitchhickers", "Guide")).toBe("Error!");
+        })*/
     });
 });
