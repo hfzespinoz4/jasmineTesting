02794f6f82706601ee2b3db8e89baa2dcb88ec67
Harvin Zúniga <harvin.zuniga@gmail.com>
2020-08-03 09:02:45 +0000

    Adding defensive programming

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,3 +1,12 @@
-function addition (){
-    return 42;
+function addition (numOne, numTwo){
+ /*This is known as defensive programming*/
+ if (typeof(numOne) == "number" && typeof(numTwo) == "number") {
+    return numOne + numTwo;
+ }  else {
+     return "Error!"
+ }
 }
+
+/*function addition (numOne, numTwo){
+    return numOne + numTwo;
+}*/
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,7 +1,13 @@
 describe("Calculator", function(){
     describe("Addition test", function (){
         it ("should return 42", function(){ 
-            expect(addition(20, 22).toBe(42));
+            expect(addition(20, 22)).toBe(42);
         });
+        it ("should return 26", function(){
+            expect(addition(7, 19)).toBe(26);
+        });
+        it ("should return error if we don´t supply two numbers", function(){
+            expect(addition("Hitchhickers", "Guide")).toBe("Error!");
+        })
     });
 });
